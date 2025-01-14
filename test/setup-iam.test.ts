10a04ffd3e2421359508c7e921de22637a361e8c
Kazuyoshi Mii <kazuyoshi.mii.wh@ntt-tx.co.jp>
2025-01-15 00:46:11 +0900

    Initial commit

---
+++ test/setup-iam.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SetupIam from '../lib/setup-iam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/setup-iam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SetupIam.SetupIamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
